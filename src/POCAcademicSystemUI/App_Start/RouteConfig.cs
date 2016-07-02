70ddbea788c17207a72a0034873df7b9e3125a2f
renanroncarati <renanroncarati@gmail.com>
2016-07-02 13:44:24 -0300

    change default route to Faculdade instead of Home controller

--- src/POCAcademicSystemUI/App_Start/RouteConfig.cs
+++ src/POCAcademicSystemUI/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace POCAcademicSystemUI
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Faculdade", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
