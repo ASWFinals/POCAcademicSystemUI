b31ba47192dd11432db4c75df9925c7194d7bc24
renanroncarati <renanroncarati@gmail.com>
2016-07-02 16:19:56 -0300

    create httprequester client to consume the api poc. closes #5

--- src/POCAcademicSystemUI/Controllers/FaculdadeController.cs
+++ src/POCAcademicSystemUI/Controllers/FaculdadeController.cs
@@ -1,16 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using POCAcademicSystemUI.Models.Contract;
+using POCAcademicSystemUI.Providers;
 
 namespace POCAcademicSystemUI.Controllers
 {
     public class FaculdadeController : Controller
     {
-        // GET: Faculdade
-        public ActionResult Index()
+        private readonly HttpRequesterProvider _httpClient;
+
+        public FaculdadeController()
         {
+            _httpClient = new HttpRequesterProvider("http://apipocacademic-staging.azurewebsites.net/api", "application/json");
+        }
+
+        // GET: Faculdade
+        public async Task<ActionResult> Index()
+        {
+            var response = _httpClient.Request("/enrollment", HttpMethod.Get);
+
+            //TODO: Try to deserialize object
+
+            //List<Enrollment> enrollments = await response.Result.Content.ReadAsAsync<List<Enrollment>>();
+
             return View();
         }
     }
---
+++ src/POCAcademicSystemUI/Providers/HttpRequesterProvider.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace POCAcademicSystemUI.Providers
+{
+    public class HttpRequesterProvider : IDisposable
+    {
+        private readonly string _baseUrl;
+        private readonly string _contentType;
+
+        public HttpRequesterProvider(string baseUrl, string contentType)
+        {
+            _baseUrl = baseUrl;
+            _contentType = contentType;
+        }
+
+        public void Dispose()
+        {
+            //TODO: implement dispose
+        }
+
+        public async Task<HttpResponseMessage> Request(string stringUri, HttpMethod method, string content = null)
+        {
+            HttpResponseMessage response;
+
+            var request = new HttpRequestMessage() {RequestUri = new Uri(_baseUrl + stringUri)};
+
+            if (!string.IsNullOrWhiteSpace(content))
+            {
+                request.Content = new StringContent(content);
+            }
+
+            if (string.IsNullOrWhiteSpace(_contentType))
+            {
+                request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            }
+
+            request.Method = method;
+
+            using (var client = new HttpClient() {Timeout = TimeSpan.FromSeconds(300)})
+            {
+                response = await client.SendAsync(request);
+            }
+
+            return response;
+        }
+    }
+}
